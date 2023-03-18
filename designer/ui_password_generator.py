5d520128e93e55cb882acf81347572a14f780a9e
FLAiRistaken <flairx@protonmail.com>
2023-03-18 22:29:33 +0000

    Copy and generate icons added to password generator

--- designer/ui_password_generator.py
+++ designer/ui_password_generator.py
@@ -15,15 +15,17 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
-    QLineEdit, QListWidget, QListWidgetItem, QPushButton,
-    QRadioButton, QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
+    QLabel, QLayout, QLineEdit, QListWidget,
+    QListWidgetItem, QPushButton, QRadioButton, QSizePolicy,
+    QToolButton, QVBoxLayout, QWidget)
+import icons_rc
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(823, 565)
+        Form.resize(873, 668)
         Form.setStyleSheet(u"QWidget#widget{\n"
 "	background-color: rgba(32, 32, 32, 255);\n"
 "	border-radius: 8px;\n"
@@ -33,7 +35,18 @@ class Ui_Form(object):
         self.widget = QWidget(Form)
         self.widget.setObjectName(u"widget")
         self.widget.setMaximumSize(QSize(800, 492))
-        self.widget.setStyleSheet(u"QFrame#frame_pass_history {\n"
+        self.widget.setStyleSheet(u"QPushButton#btnClose{\n"
+"	color:rgba(255, 255, 255, 220);\n"
+"}\n"
+"QPushButton#btnClose:pressed{\n"
+"	color: rgba(126, 126, 126, 220);\n"
+"\n"
+"}\n"
+"QPushButton#btnClose:hover{\n"
+"	color: rgba(190, 190, 190, 220);\n"
+"}\n"
+"\n"
+"QFrame#frame_pass_history {\n"
 "	background-color: rgb(23, 23, 23);\n"
 "	border-radius: 8px;\n"
 "}\n"
@@ -44,7 +57,36 @@ class Ui_Form(object):
 "QFrame#frame_options{\n"
 "	background-color: rgb(23, 23, 23);\n"
 "	border-radius: 8px;\n"
-"}")
+"}\n"
+"\n"
+"QToolButton#btn_gen_pass {\n"
+"	background-color: rgb(23, 23, 23);\n"
+"	border: none;\n"
+"}\n"
+"\n"
+"QToolButton#btn_copy {\n"
+"	background-color: rgb(23, 23, 23);\n"
+"	border-top-right-radius: 5px;\n"
+"	border-bottom-right-radius: 5px;\n"
+"}\n"
+"\n"
+"QToolButton#btn_copy:hover{\n"
+"	background-color: rgb(35, 35, 35);\n"
+"}\n"
+"\n"
+"QToolButton#btn_gen_pass:hover{\n"
+"	background-color: rgb(35, 35, 35);\n"
+"}\n"
+"\n"
+"QToolButton#btn_copy:pressed{\n"
+"	background-color: rgb(40, 40, "
+                        "40);\n"
+"}\n"
+"\n"
+"QToolButton#btn_gen_pass:pressed{\n"
+"	background-color: rgb(40, 40, 40);\n"
+"}\n"
+"")
         self.gridLayout_2 = QGridLayout(self.widget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.btnClose = QPushButton(self.widget)
@@ -111,28 +153,15 @@ class Ui_Form(object):
         self.lbl_options.setMaximumSize(QSize(508, 17))
         self.lbl_options.setFont(font1)
 
-        self.gridLayout_3.addWidget(self.lbl_options, 1, 0, 1, 1)
-
-        self.le_password = QLineEdit(self.widget)
-        self.le_password.setObjectName(u"le_password")
-        sizePolicy4 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
-        sizePolicy4.setHorizontalStretch(0)
-        sizePolicy4.setVerticalStretch(0)
-        sizePolicy4.setHeightForWidth(self.le_password.sizePolicy().hasHeightForWidth())
-        self.le_password.setSizePolicy(sizePolicy4)
-        self.le_password.setMinimumSize(QSize(0, 45))
-        self.le_password.setMaximumSize(QSize(508, 45))
-        self.le_password.setStyleSheet(u"background-color: rgb(23, 23, 23);\n"
-"border-radius: 5px")
-        self.le_password.setReadOnly(True)
-        self.le_password.setClearButtonEnabled(True)
-
-        self.gridLayout_3.addWidget(self.le_password, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.lbl_options, 4, 0, 1, 1)
 
         self.frame_pass_type = QFrame(self.widget)
         self.frame_pass_type.setObjectName(u"frame_pass_type")
-        sizePolicy3.setHeightForWidth(self.frame_pass_type.sizePolicy().hasHeightForWidth())
-        self.frame_pass_type.setSizePolicy(sizePolicy3)
+        sizePolicy4 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.frame_pass_type.sizePolicy().hasHeightForWidth())
+        self.frame_pass_type.setSizePolicy(sizePolicy4)
         self.frame_pass_type.setMinimumSize(QSize(0, 75))
         self.frame_pass_type.setMaximumSize(QSize(508, 75))
         self.frame_pass_type.setFrameShape(QFrame.StyledPanel)
@@ -157,7 +186,55 @@ class Ui_Form(object):
         self.gridLayout_4.addWidget(self.rad_passphrase, 1, 1, 1, 1)
 
 
-        self.gridLayout_3.addWidget(self.frame_pass_type, 2, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.frame_pass_type, 5, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.le_password = QLineEdit(self.widget)
+        self.le_password.setObjectName(u"le_password")
+        sizePolicy4.setHeightForWidth(self.le_password.sizePolicy().hasHeightForWidth())
+        self.le_password.setSizePolicy(sizePolicy4)
+        self.le_password.setMinimumSize(QSize(0, 45))
+        self.le_password.setMaximumSize(QSize(448, 45))
+        font2 = QFont()
+        font2.setPointSize(16)
+        self.le_password.setFont(font2)
+        self.le_password.setStyleSheet(u"background-color: rgb(23, 23, 23);\n"
+"border-top-left-radius: 5px;\n"
+"border-bottom-left-radius: 5px;\n"
+"padding-bottom:1px;\n"
+"padding-left:3px")
+        self.le_password.setReadOnly(True)
+        self.le_password.setClearButtonEnabled(False)
+
+        self.horizontalLayout.addWidget(self.le_password)
+
+        self.btn_gen_pass = QToolButton(self.widget)
+        self.btn_gen_pass.setObjectName(u"btn_gen_pass")
+        self.btn_gen_pass.setMaximumSize(QSize(30, 45))
+        self.btn_gen_pass.setStyleSheet(u"")
+        icon = QIcon()
+        icon.addFile(u":/pass_gen/refresh.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_gen_pass.setIcon(icon)
+        self.btn_gen_pass.setIconSize(QSize(20, 20))
+
+        self.horizontalLayout.addWidget(self.btn_gen_pass)
+
+        self.btn_copy = QToolButton(self.widget)
+        self.btn_copy.setObjectName(u"btn_copy")
+        self.btn_copy.setMaximumSize(QSize(30, 45))
+        self.btn_copy.setStyleSheet(u"")
+        icon1 = QIcon()
+        icon1.addFile(u":/pass_gen/copypaste_white.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_copy.setIcon(icon1)
+        self.btn_copy.setIconSize(QSize(20, 20))
+
+        self.horizontalLayout.addWidget(self.btn_copy)
+
+
+        self.gridLayout_3.addLayout(self.horizontalLayout, 2, 0, 1, 1)
 
 
         self.gridLayout_2.addLayout(self.gridLayout_3, 1, 0, 2, 1)
@@ -167,6 +244,7 @@ class Ui_Form(object):
 
 
         self.retranslateUi(Form)
+        self.btn_copy.clicked.connect(self.le_password.copy)
 
         QMetaObject.connectSlotsByName(Form)
     # setupUi
@@ -179,5 +257,7 @@ class Ui_Form(object):
         self.rad_password.setText(QCoreApplication.translate("Form", u"Password", None))
         self.lbl_pass_type.setText(QCoreApplication.translate("Form", u"Password Type", None))
         self.rad_passphrase.setText(QCoreApplication.translate("Form", u"Passphrase", None))
+        self.btn_gen_pass.setText("")
+        self.btn_copy.setText("")
     # retranslateUi
 
