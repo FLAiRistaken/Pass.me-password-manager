564c9748173c31d1064d58def3cb226aa524b012
FLAiRistaken <flairx@protonmail.com>
2023-03-18 17:23:04 +0000

    Passwords now generate with desired chars correctly. Special characters changed

--- package/password_generator.py
+++ package/password_generator.py
@@ -7,6 +7,8 @@ class GenPassword():
         self.numbers = numbers
         self.special = special
         self.caps = caps
+        
+        self.punctuation = ".!@#$%^&*"
     
     def create_alphabet(self) -> str:
         alphabet = string.ascii_lowercase
@@ -17,7 +19,7 @@ class GenPassword():
         if self.numbers:
             alphabet += string.digits
         if self.special:
-            alphabet += (string.punctuation).replace("'", "").replace('"', "").replace("@", "").replace('|', "") # Removing certain unwanted speical characters
+            alphabet += self.punctuation
         return alphabet
 
     def generate_password(self) -> str:
@@ -27,11 +29,14 @@ class GenPassword():
             password = ''.join(secrets.choice(alphabet) for i in range(self.length))
             if any(c.islower() for c in password):
                 if self.caps:
-                    any(c.isupper() for c in password)
+                    if not (any(c.isupper() for c in password)):
+                        continue
                 if self.numbers:
-                    any(c.isdigit() for c in password)
+                    if not (any(c.isdigit() for c in password)):
+                        continue
                 if self.special:
-                    any(c in string.punctuation for c in password)
+                    if not (any(c in self.punctuation for c in password)):
+                        continue
                 break
         return password
 
