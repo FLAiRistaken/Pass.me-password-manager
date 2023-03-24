fd2669db8586ecb8c46a4406c2bdf673bf8c9903
FLAiRistaken <flairx@protonmail.com>
2023-03-24 20:50:16 +0000

    Changed tags to folder

--- package/model/Items.py
+++ package/model/Items.py
@@ -1,14 +1,14 @@
 class GeneralItem():
-    def __init__(self, note=None, tags=None):
+    def __init__(self, note=None, folder=None):
         self.note = note
-        self.tags = tags
+        self.folder = folder
 
 
 class LoginItem(GeneralItem):
-    def __init__(self, email=None, password=None, website=None, note=None, tags=None):
-        super().__init__(note, tags)
+    def __init__(self, email=None, password=None, website=None, note=None, folder=None):
+        super().__init__(note, folder)
         self.email = email
         self.password = password
         self.website = website
         self.note = note
-        self.tags = tags
+        self.folder = folder
