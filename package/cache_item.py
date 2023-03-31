7ebced3c9dd37578c3299c372a08c46a76455a14
FLAiRistaken <flairx@protonmail.com>
2023-03-31 20:30:05 +0100

    All item types can now be cached

--- package/cache_item.py
+++ package/cache_item.py
@@ -31,6 +31,59 @@ class CacheItem():
                 "folder": item.folder,
                 "type": "login"
             }
+        elif isinstance(item, BankAccItem):
+            return {
+                "name": item.name,
+                "name_on_account": item.name_on_account,
+                "account_number": item.account_number,
+                "sort_code": item.sort_code,
+                "date_created": item.date_created,
+                "date_modified": item.date_modified,
+                "note": item.note,
+                "folder": item.folder,
+                "type": "bank_acc"
+            }
+        elif isinstance(item, BankCardItem):
+            return {
+                "name": item.name,
+                "name_on_card": item.name_on_card,
+                "card_number": item.card_number,
+                "exp_month": item.exp_month,
+                "exp_year": item.exp_year,
+                "brand": item.brand,
+                "cvv": item.cvv,
+                "date_created": item.date_created,
+                "date_modified": item.date_modified,
+                "note": item.note,
+                "folder": item.folder,
+                "type": "bank_card"
+            }
+        elif isinstance(item, IdentityItem):
+            return {
+                "name": item.name,
+                "title": item.title,
+                "first_name": item.first_name,
+                "last_name": item.last_name,
+                "email": item.email,
+                "phone_number": item.phone_number,
+                "nat_insur_no": item.nat_insur_no,
+                "pass_no": item.pass_no,
+                "license_no": item.license_no,
+                "date_created": item.date_created,
+                "date_modified": item.date_modified,
+                "note": item.note,
+                "folder": item.folder,
+                "type": "identity"
+            }
+        elif isinstance(item, SecureNoteItem):
+            return {
+                "name": item.name,
+                "note": item.note,
+                "date_created": item.date_created,
+                "date_modified": item.date_modified,
+                "folder": item.folder,
+                "type": "secure_note"
+            }
         else:
             return {
                 "name": item.name,
@@ -92,6 +145,24 @@ class CacheItem():
                 return LoginItem(item_json["name"], item_json["email"], item_json["password"],
                                  item_json["website"], item_json["date_created"],
                                  item_json["date_modified"], item_json["note"], item_json["folder"])
+            case "bank_acc":
+                return BankAccItem(item_json["name"], item_json["name_on_account"], item_json["account_number"],
+                                    item_json["sort_code"], item_json["date_created"], item_json["date_modified"],
+                                    item_json["note"], item_json["folder"])
+            case "bank_card":
+                return BankCardItem(item_json["name"], item_json["name_on_card"], item_json["card_number"],
+                                    item_json["exp_month"], item_json["exp_year"], item_json["brand"],
+                                    item_json["cvv"], item_json["date_created"], item_json["date_modified"],
+                                    item_json["note"], item_json["folder"])
+            case "identity":
+                return IdentityItem(item_json["name"], item_json["title"], item_json["first_name"],
+                                    item_json["last_name"], item_json["email"], item_json["phone_number"],
+                                    item_json["nat_insur_no"], item_json["pass_no"], item_json["license_no"],
+                                    item_json["date_created"], item_json["date_modified"], item_json["note"],
+                                    item_json["folder"])
+            case "secure_note":
+                return SecureNoteItem(item_json["name"], item_json["note"], item_json["date_created"],
+                                        item_json["date_modified"], item_json["folder"])
             case "general":
                 return GeneralItem(item_json["name"], item_json["date_created"], item_json["date_modified"],
                                    item_json["note"], item_json["folder"])
